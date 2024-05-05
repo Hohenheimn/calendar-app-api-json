93e7015722845a30fd274b4cd9c58028517c420c
Jomari Tiu <85975045+Hohenheimn@users.noreply.github.com>
2024-05-05 13:07:49 +0800

    add:custom login route

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
-const jsonServer = require("json-server"); // importing json-server library
-const server = jsonServer.create();
+const express = require("express");
+const jsonServer = require("json-server");
+const server = express(); // Using express instead of json-server directly
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000; //  chose port from here like 8080, 3001
+const port = process.env.PORT || 3000;
 
+server.use(express.json());
+server.use(express.urlencoded({ extended: false }));
 server.use(middlewares);
-server.use(router);
+server.use("/api", router); // Mounting JSON Server router under /api
 
-server.listen(port);
+server.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  const db = require("./db.json");
+  const user = db.users.find(
+    (user) => user.email === email && user.password === password
+  );
+
+  if (user) {
+    res
+      .status(200)
+      .json({ message: "Login successful", user, accessToken: "token" });
+  } else {
+    res.status(401).json({ error: "Invalid email or password" });
+  }
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
